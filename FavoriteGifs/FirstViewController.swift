3ca3dcbb4ed9d91510d0f4d359e68d7ca1451775
Jihyun Son <sonjh1217@naver.com>
2020-03-17 22:28:28 +0900

    Initial Commit

---
+++ FavoriteGifs/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FavoriteGifs
+//
+//  Created by Jihyun Son on 2020/03/17.
+//  Copyright © 2020 Jihyun Son. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FavoriteGifs/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FavoriteGifs
+//
+//  Created by Jihyun Son on 2020/03/17.
+//  Copyright © 2020 Jihyun Son. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
