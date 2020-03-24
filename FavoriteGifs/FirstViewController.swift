0abb59fd2addf3bc59e6c093d3798e468a6a54a7
Jihyun Son <sonjh1217@naver.com>
2020-03-24 20:44:10 +0900

    기본 뷰 세팅

--- FavoriteGifs/FirstViewController.swift
+++ FavoriteGifs/FirstViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 1. searchBar 붙이기
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(searchController.searchBar)
+        
+        NSLayoutConstraint.activate([
+            self.view.safeAreaLayoutGuide.topAnchor.constraint(equalTo: searchController.searchBar.topAnchor),
+            self.view.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: searchController.searchBar.leadingAnchor),
+            self.view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: searchController.searchBar.trailingAnchor)
+        ])
+        
+        // 2. collectionView 붙이기
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: searchController.searchBar.bottomAnchor),
+            self.view.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: collectionView.leadingAnchor),
+            self.view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: collectionView.trailingAnchor),
+            self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: collectionView.bottomAnchor)
+        ])
+        
+        // 3. searchResultUpdate 설정
+        searchController.searchResultsUpdater = self
+        
     }
 
 
+}
+
+extension FirstViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        // TODO: 지현 - API 호출
+        
+        
+    }
 }
 
